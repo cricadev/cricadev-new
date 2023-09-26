b1dd4fe94b8373fab43c641a5f0fb5132abfb565
CristianN998 <cristiancasallas998@gmail.com>
2023-09-26 17:47:15 -0500

    nuxt image and sass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ["@nuxt/ui", "@nuxt/image"],
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"]
+  image: {
+    cloudinary: {
+      baseURL: "https://res.cloudinary.com/ddc0cce3m/image/upload/v1695676230/CricaDev/",
+    },
+  },
+
 })
